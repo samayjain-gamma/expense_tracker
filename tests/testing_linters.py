fb259885e60ee313cad617d3a0052f8dd1dba4f2
Samay Jain <samay.jain@gammaedge.io>
2026-02-06 16:19:11 +0530

    logger and exception handler added

---
+++ tests/testing_linters.py
@@ -0,0 +1,14 @@
+# Write a messy code here
+# Code is commented, so that mypy and flake8 won't raise an error
+"""
+import os
+
+from app import api
+
+
+def func(a, c, b=2):
+    return a + b * c
+
+
+func(2, 3)
+"""
